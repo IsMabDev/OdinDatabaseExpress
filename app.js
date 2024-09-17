ff852963fa3d3ffda6249d55402aca23853d7801
issam <145654259+IsMabDev@users.noreply.github.com>
2024-09-17 09:50:13 +0100

    prepare to deploy to render.com changing the package.json

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require("express")
-const app = express()
-const userRoutes=require("./routes/userRoutes")
-app.use("/",userRoutes);
-
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+app.use("/", userRoutes);
+console.log("my test envirenment",process.env.TEST_ENV);
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
