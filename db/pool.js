5ccdc258a6b0ef090c20782f942cf6a02b23bb16
issam <145654259+IsMabDev@users.noreply.github.com>
2024-09-19 07:24:33 +0100

    Chage the pool to connect to render

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,26 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
-module.exports = new Pool({
-  host: process.env.DB_HOST, // or wherever the db is hosted
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT, // The default port
+const userName=process.env.DB_USER
+const password=process.env.DB_PASSWORD
+const hostname=process.env.DB_HOST
+const port=process.env.DB_PORT
+const database=process.env.DB_NAME
+const DATABASE_URL=`postgres://${userName}:${password}@${hostname}:${port}/${database}`
+// Use the full connection URI from Render
+const pool = new Pool({
+  connectionString: DATABASE_URL, // DATABASE_URL from Render
+  ssl: {
+    rejectUnauthorized: false, // This is necessary for secure connection to Render PostgreSQL
+  },
 });
+
+// Example query
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    console.log("Connected to the database:", res.rows);
+  }
+});
+
+module.exports = pool;
